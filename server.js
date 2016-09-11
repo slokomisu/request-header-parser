b2676d377dac20d1f35036b0768068cc6370c2b0
Noah J. Guillory <slokomisu4@gmail.com>
2016-09-11 12:54:41 -0500

    Finish whoami parser

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var useragent = require('express-useragent');
+var app = express();
+app.use(useragent.express());
+
+app.get('/api/whoami', (req, res) => {
+    var agent = req.useragent['source'].split('(');
+    var ip = req.ip;
+    var os = agent[1].split(')')[0];
+    var lang = req.get('Accept-Language').split(',')[0];
+
+    var whoami = {
+        ipaddress: ip,
+        language: lang,
+        software: os
+    };
+    res.json(whoami);
+
+
+})
+app.listen(8080);
